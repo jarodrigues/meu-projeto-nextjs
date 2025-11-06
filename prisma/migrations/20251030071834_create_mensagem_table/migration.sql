3afc303a5ea5bea284f9bda45812fb38b6e9c4d3
jonasrodrigues <jonasgroover@gmail.com>
2025-11-06 02:17:55 -0300

    up migration too

--- prisma/migrations/20251030071834_create_mensagem_table/migration.sql
+++ prisma/migrations/20251030071834_create_mensagem_table/migration.sql
@@ -1,10 +1,10 @@
 -- CreateTable
-CREATE TABLE "Mensagem" (
+CREATE TABLE "Message" (
     "id" SERIAL NOT NULL,
     "nome" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "mensagem" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Mensagem_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
 );
