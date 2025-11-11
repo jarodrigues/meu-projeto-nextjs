ad02b6be1944d90ba0bdd9997e9e43ff5f1e34b1
jonasrodrigues <jonasgroover@gmail.com>
2025-11-11 00:00:44 -0300

    New Fix Prisma engine on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    // Api routes
+    "/api/**/*": ["./node_modules/.prisma/client/**/*"],
+    // server components / server actions
+    "/*": ["./node_modules/.prisma/client/**/*"],
+  },
 };
 
 export default nextConfig;
